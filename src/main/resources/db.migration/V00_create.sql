13f7f3172aee80b0644970b9757330b44b95202e
rafael.santos <rafael.santos@compasso.com.br>
2021-06-07 13:45:30 -0300

    Criação da tabela users

--- src/main/resources/db.migration/V00_create.sql
+++ src/main/resources/db.migration/V00_create.sql
@@ -1,7 +1,11 @@
-CREATE TABLE users (
-  name NVarChar(255) COLLATE utf8_general_ci,
-  id
-
-  PRIMARY KEY (
-      id
+CREATE TABLE users.users (
+  id bigint auto_increment NOT NULL,
+  name NVarChar(255) varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  document NVarChar(255) varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  phone longtext NOT NULL,
+  adress NVarChar(255) varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  cep longtext NOT NULL,
+  password NVarChar(255) varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (id)
   )
+
